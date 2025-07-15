a140a3eb1c1d3ead2f3ce435bd8db3b01c687275
Doria Keung <doria.keung@gmail.com>
2025-07-15 18:40:01 -0400

    Fix lint

--- example_test.go
+++ example_test.go
@@ -17,11 +17,11 @@ package hyperpb_test
 import (
 	"fmt"
 
+	weatherv1 "buf.build/gen/go/bufbuild/hyperpb-examples/protocolbuffers/go/example/weather/v1"
 	"buf.build/go/protovalidate"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/descriptorpb"
 
-	weatherv1 "buf.build/gen/go/bufbuild/hyperpb-examples/protocolbuffers/go/example/weather/v1"
 	"buf.build/go/hyperpb"
 	"buf.build/go/hyperpb/internal/examples"
 )
